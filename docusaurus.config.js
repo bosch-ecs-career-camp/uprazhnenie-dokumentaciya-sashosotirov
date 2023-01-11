837b10a6631a03abba7462ffc932cca8197c6c8a
sashosotirov <sotirov.sasho@gmail.com>
2023-01-11 14:43:34 +0200

    new update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Document pages',
+  title: 'Document pages',
   tagline: 'Dinosaurs are cool',
   url: 'https://bosch-ecs-career-camp.github.io',
   baseUrl: 'uprazhnenie-dokumentaciya-sashosotirov',
